0a26d9c94653bd0604afbd925d4a6563ef470733
ingridsj <ingridsjalmeida@gmail.com>
2023-04-22 17:44:09 -0300

    add botão de adicionar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,27 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log('botão add')
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
 
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
 
-      <TextInput 
+      <TextInput
         style={styles.input}
         placeholder="Nome do participante"
         placeholderTextColor="#6B6B6B"
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
